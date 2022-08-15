78946c0d9a960d275095f309da50ccc7efa1c729
truix12 <truix12@gmail.com>
2022-08-15 14:49:28 +0200

    clean CSS and upgrade JS

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-const btnOffer = document.querySelector(".btn-offer");
-const btnExecution = document.querySelector(".btn-execution");
-const btnContact = document.querySelector(".btn-contact");
+const btn = document.querySelectorAll('li a');
 
 const offerDiv = document.querySelector(".offer");
 const executionDiv = document.querySelector(".execution")
 const contactDiv = document.querySelector(".footer-div")
 
-btnOffer.addEventListener("click", myFunctionOffer)
+let bo = btn[0];
+let be = btn[1];
+let bc = btn[2];
 
-function myFunctionOffer() {
-  offerDiv.scrollIntoView( {
-    behavior: 'smooth'
-  });
-}
-
-btnExecution.addEventListener("click", myFunctionExecution)
-
-function myFunctionExecution() {
-  executionDiv.scrollIntoView( {
-    behavior: 'smooth'
-  });
-}
-
-btnContact.addEventListener("click", myFunctionContact)
-
-function myFunctionContact() {
-  contactDiv.scrollIntoView( {
-    behavior: 'smooth'
-  });
+for (const b of btn) {
+  b.addEventListener("click", function() {
+    if (b.className == "btn-offer") {
+      offerDiv.scrollIntoView( {
+        behavior: 'smooth'
+      });
+    }
+    if (b.className == "btn-execution") {
+      executionDiv.scrollIntoView( {
+        behavior: 'smooth'
+      });
+    }
+    else if (b.className == "btn-contact") {
+      contactDiv.scrollIntoView( {
+        behavior: 'smooth'
+      });
+    }
+  })
 }
